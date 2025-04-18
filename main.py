8f57c15d0fbd328b311528f576ad95b5d1cf690e
Alex_Roberts46 <noinsong46@gmail.com>
2025-04-17 22:45:19 -0500

    bookbot can now count any book

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ def get_book_text(book):
         return file_contents
 
 def main():
-    book = "./books/frankenstein.txt"
-    from stats import get_num_words
-    from stats import counting_characters
-    from stats import character_sort
-    book_text = get_book_text(book)
-    num_words = get_num_words(book_text)
-    characters = counting_characters(book_text)
-    sorted = character_sort(characters)
-    print(f"============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
-    print(f"----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print(f"--------- Character Count -------")
-    for char_dict in sorted:
-        print (f"{char_dict["letter"]}: {char_dict["count"]}")
-    print(f"============= END ===============")
+    import sys
+
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        from stats import get_num_words
+        from stats import counting_characters
+        from stats import character_sort
+        book = sys.argv[1]
+        book_text = get_book_text(book)
+        num_words = get_num_words(book_text)
+        characters = counting_characters(book_text)
+        sorted = character_sort(characters)
+        print(f"============ BOOKBOT ============")
+        print(f"Analyzing book found at {book}...")
+        print(f"----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print(f"--------- Character Count -------")
+        for char_dict in sorted:
+            print (f"{char_dict["letter"]}: {char_dict["count"]}")
+        print(f"============= END ===============")
 
 main()
